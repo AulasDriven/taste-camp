0e45c768f3bafdb8511d78150aa8c3bfa6c47b1c
Diego Martins de Pinho <diegomartinsdepinho@msn.com>
2022-02-02 12:05:50 -0300

    base bem melhor estruturada

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,60 @@
 const receitas = [{
-    titulo: "Ovo frito", ingredientes: "Ovo, óleo e sal", preparo: "Frita o ovo ae"
+  titulo: "Ovo frito",
+  ingredientes: "Ovo, óleo e sal",
+  preparo: "Oléo da frigideira e fogo baixo no fogão. Quebra o ovo na manhazinha e taca na frigideira. Joga um salzinho pra dar um gostinho top e espera um pouquinho até fritar."
 }];
 
-// Populando menu
-function renderizarReceitas() {
-    const ulReceitas = document.querySelector(".receitas");
-    ulReceitas.innerHTML = "";
+const paginaAdicionar = document.querySelector('.pagina.adicionar-receita');
+const paginaVerReceita = document.querySelector('.pagina.receita');
 
-    for(let i = 0; i < receitas.length; i++) {
-        ulReceitas.innerHTML += `
-            <li>
-                <ion-icon name="fast-food-outline"></ion-icon>
-                ${receitas[i].titulo}
-            </li>   
-        `;
-    }
+function renderizarReceitasMenu() {
+  const ulReceitas = document.querySelector(".receitas");
+  ulReceitas.innerHTML = "";
+  for (let i = 0; i < receitas.length; i++) {
+    ulReceitas.innerHTML += `
+        <li onClick='renderizarDetalhesReceita(${i})'>
+            <ion-icon name="fast-food-outline"></ion-icon>
+            ${receitas[i].titulo}
+        </li>   
+    `;
+  }
+}
+
+function renderizarDetalhesReceita(indice) {  
+  mostrarReceitaPagina();
+  const tituloHTML = document.querySelector('.receita .titulo');
+  const ingredientesHTML = document.querySelector('.receita .ingredientes .destaque');
+  const modoPreparoHTML = document.querySelector('.receita .modo-de-preparo .destaque');
+
+  tituloHTML.innerHTML = receitas[indice].titulo;
+  ingredientesHTML.innerHTML = receitas[indice].ingredientes;
+  modoPreparoHTML.innerHTML = receitas[indice].preparo;
 }
 
 function adicionarReceita() {
-    const titulo = document.querySelector(".nome-receita").value;
-    const ingredientes = document.querySelector(".ingredientes-receita").value;
-    const preparo = document.querySelector(".modo-preparo-receita").value;
+  const titulo = document.querySelector(".nome-receita").value;
+  const ingredientes = document.querySelector(".ingredientes-receita").value;
+  const preparo = document.querySelector(".modo-preparo-receita").value;
 
+  if(titulo && ingredientes && preparo) {
     const novaReceita = { titulo: titulo, ingredientes: ingredientes, preparo: preparo };
     receitas.push(novaReceita);
-    renderizarReceitas();
+    renderizarReceitasMenu();
+  } else {
+    alert("Precisa preencher tudo bonitinho!"); 
+  }
+
 }
 
-renderizarReceitas();
+function adicionarReceitaPagina() {
+  paginaAdicionar.classList.remove('escondido');
+  paginaVerReceita.classList.add('escondido');
+}
+
+function mostrarReceitaPagina() {
+  paginaAdicionar.classList.add('escondido');
+  paginaVerReceita.classList.remove('escondido');
+}
+
+renderizarReceitasMenu();
+renderizarDetalhesReceita(0);
